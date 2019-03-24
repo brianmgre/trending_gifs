ccadb3500072abc3411ab08e6a99373320439eef
Brian G <brian.gregorius@gmail.com>
2019-03-24 18:59:20 -0400

    passed through index prop

--- src/components/gif.js
+++ src/components/gif.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import GifModal from "./gifModal";
+import Favorites from "./favorites";
 
 class Gif extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      favorites: []
     };
   }
 
@@ -17,13 +19,33 @@ class Gif extends Component {
     this.setState({ open: !this.state.open });
   };
 
+  // AddToFavorites = event => {
+  //   event.preventDefault();
+  //   this.setState({
+  //     favorites: [
+  //       ...this.state.favorites,
+  //       {
+  //         title: this.props.gif.title,
+  //         fixed_width: this.props.gif.images.fixed_width.url,
+  //         fixed_width_still: this.props.gif.images.fixed_width_still.url,
+  //         rating: this.props.gif.rating,
+  //         original: this.props.gif.images.original.url,
+  //         import_datetime: this.props.gif.import_datetime
+  //       }
+  //     ]
+  //   });
+  //   localStorage.setItem("favorites", this.state.favorites);
+  // };
+
   render() {
     return (
       <div>
         <GifModal
+          AddToFavorites={this.props.AddToFavorites}
           open={this.state.open}
           close={this.close}
           gif={this.props.gif}
+          index={this.props.index}
         />
         {this.props.gifsOn ? (
           <img
