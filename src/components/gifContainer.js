b1b9482e9beab6752decef9f96fe584e26d4c243
Brian G <brian.gregorius@gmail.com>
2019-03-24 18:58:57 -0400

    add method add to favorites and a way to update them to local storage

--- src/components/gifContainer.js
+++ src/components/gifContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import GifList from "./gifList";
 import ToggleForm from "./toggleForm";
 import Search from "./search";
-import Sort from "./sort";
+import Favorites from "./favorites";
 
 const giphyApi = process.env.REACT_APP_API;
 const GphApiClient = require("giphy-js-sdk-core");
@@ -16,7 +16,9 @@ class GifContainer extends Component {
       gifs: [],
       gifsOn: false,
       searchResults: [],
-      searchTerm: ""
+      searchTerm: "",
+      favorites: [],
+      showFavorites: true
     };
   }
 
@@ -29,6 +31,12 @@ class GifContainer extends Component {
           this.setState({ gifs: res.data });
         }
       })
+      .then(res => {
+        if (localStorage.getItem("favorites")) {
+          const storedFav = JSON.parse(localStorage["favorites"]);
+          this.setState({ favorites: storedFav });
+        }
+      })
       .catch(err => {
         console.log(err, "error");
       });
@@ -36,7 +44,7 @@ class GifContainer extends Component {
 
   searchForGif = event => {
     event.preventDefault();
-    if (this.state.searchTerm != "") {
+    if (this.state.searchTerm !== "") {
       client
         .search(`gifs`, { q: `${this.state.searchTerm}` })
         .then(res => {
@@ -63,14 +71,33 @@ class GifContainer extends Component {
   componentDidUpdate(preProps, preState) {
     if (preState.searchTerm !== this.state.searchTerm) {
       this.setState({ searchResults: [] });
+    } else if (preState.favorites !== this.state.favorites) {
+      localStorage["favorites"] = JSON.stringify(this.state.favorites);
     }
   }
+
+  AddToFavorites = index => event => {
+    event.preventDefault();
+    if (index) {
+      this.setState({
+        favorites: [
+          ...this.state.favorites,
+          {
+            id: this.state.gifs[index].id,
+            title: this.state.gifs[index].title,
+            original: this.state.gifs[index].images.original.url,
+            rating: this.state.gifs[index].rating,
+            import_datetime: this.state.gifs[index].import_datetime
+          }
+        ]
+      });
+    }
+  };
 
   render() {
     console.log("gif state", this.state);
     return (
       <div>
-        <Sort />
         <ToggleForm
           handleChange={this.handleChange}
           gifsOn={this.state.gifsOn}
@@ -82,12 +109,17 @@ class GifContainer extends Component {
         />
         <GifList
           gifs={
-            this.state.searchTerm && this.state.searchResults.length > 0
+            this.state.searchResults.length > 0
               ? this.state.searchResults
               : this.state.gifs
           }
           gifsOn={this.state.gifsOn}
+          AddToFavorites={this.AddToFavorites}
         />
+        {/* <Favorites
+          favorites={this.state.favorites}
+          AddToFavorites={this.AddToFavorites}
+        /> */}
       </div>
     );
   }
