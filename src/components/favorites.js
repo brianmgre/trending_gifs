bdfcf7706cb87e4f62ea6bd9dc57ef8c8bab3dd3
Brian G <brian.gregorius@gmail.com>
2019-03-24 18:57:52 -0400

    added a page for favorite gifs

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
+import GifList from "./gifList";
 
 class Favorites extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      favGif: []
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("favorites")) {
+      const storedFav = JSON.parse(localStorage["favorites"]);
+      this.setState({ favGif: storedFav });
+    }
   }
 
   render() {
-    return <div>hey</div>;
+    if (this.state.favGif.length === 0) {
+      return <h1>loading!</h1>;
+    }
+    console.log(this.state);
+    return (
+      <div>
+        Your Favorite Gifs!
+        {this.state.favGif.map((fav, index) => (
+          <div key={index}>hi</div>
+        ))}
+      </div>
+    );
   }
 }
 
