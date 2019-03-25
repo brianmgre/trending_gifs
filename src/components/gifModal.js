48523043162bca6af84526373fffc955689ffbe9
Brian G <brian.gregorius@gmail.com>
2019-03-25 13:59:08 -0400

    add a way for users to share gifs

--- src/components/gifModal.js
+++ src/components/gifModal.js
@@ -2,6 +2,14 @@ import React from "react";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import { DialogContent } from "@material-ui/core";
+import {
+  FacebookIcon,
+  TwitterIcon,
+  TwitterShareButton,
+  EmailShareButton,
+  FacebookShareButton,
+  EmailIcon
+} from "react-share";
 
 const GifModal = props => {
   const { classes, close, AddToFavorites, ...other } = props;
@@ -18,6 +26,23 @@ const GifModal = props => {
       <DialogContent>
         <img src={props.gif.images.original.url} alt={props.gif.title} />
       </DialogContent>
+      <FacebookShareButton
+        quote={props.gif.title}
+        url={props.gif.images.original.url}
+        round={true}
+      >
+        <FacebookIcon size={32} round={true} />
+      </FacebookShareButton>
+      <TwitterShareButton url={props.gif.images.original.url}>
+        <TwitterIcon size={32} round={true} />
+      </TwitterShareButton>
+      <EmailShareButton
+        url={props.gif.images.original.url}
+        subject={props.gif.title}
+        image={props.gif.images.original.url}
+      >
+        <EmailIcon size={32} round={true} />
+      </EmailShareButton>
       Rating: {props.gif.rating.toUpperCase()}
     </Dialog>
   );
