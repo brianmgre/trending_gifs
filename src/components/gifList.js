a415fa49cd298eb48bc1ac771758d7d9fd447882
Brian G <brian.gregorius@gmail.com>
2019-03-24 23:13:44 -0400

    removed index

--- src/components/gifList.js
+++ src/components/gifList.js
@@ -1,25 +1,16 @@
 import React from "react";
 import Gif from "./gif";
-import Grid from "@material-ui/core/Grid";
 import { GridList, GridListTile, DialogTitle } from "@material-ui/core";
 
 const GifList = props => {
+  console.log(props);
   const { classes } = props;
   return (
-    // <Grid container spacing={24}>
-    //   {props.gifs.map(gif => (
-    //     <Grid item lg={2} xs={6} md={6} key={gif.id}>
-    //       <Gif gif={gif} gifsOn={props.gifsOn} />
-    //     </Grid>
-    //   ))}
-    // </Grid>
-
-    <GridList cellHeight={160} cols={5}>
+    <GridList cellHeight={160} cols={6}>
       {props.gifs.map((gif, index) => (
         <GridListTile cols={1} key={gif.id}>
           <Gif
             gif={gif}
-            index={index}
             gifsOn={props.gifsOn}
             AddToFavorites={props.AddToFavorites}
           />
